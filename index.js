6d0110e5fb56ba41ed20cc89b39ab1066a9b7ef5
alessandropitocchi <110122679+alessandropitocchi@users.noreply.github.com>
2022-10-15 20:45:51 +0200

    validate password, method get

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const form = document.querySelector('.form')
 
 form.addEventListener('submit', (e) => {
-    const password = document.getElementById('password')
-    const confpassword = document.getElementById('conf-password')
+    const password = document.getElementById('password').value
+    const confpassword = document.getElementById('conf-password').value
     const errormsg = document.querySelector(".psw-msg")
 
     if(password !== confpassword){
-       errormsg.textContent = "Passwords doesn't match"
+        errormsg.classList.add('error')
+        errormsg.textContent = "Passwords doesn't match"
     } 
-    
+
 })
